89cf34461c72fe5343fa02242b3be5634184daa1
hadjirozay <ojewandejamiu2016@gmail.com>
2023-10-06 14:40:43 -0700

    4 put added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main- Entry point
- *
- * Return: Always 0 (success)
- * */
+ * main - To print using puts
+ * Return: 0, if successful
+ */
+
 int main(void)
-{     puts("\"Programming is like building a multilingual puzzle");
-      return(0);
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
