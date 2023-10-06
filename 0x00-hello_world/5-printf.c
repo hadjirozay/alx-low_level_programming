8c8cc3e8779b791a29ff245c6bcf695996219ff4
hadjirozay <ojewandejamiu2016@gmail.com>
2023-10-06 15:02:20 -0700

    5-printf.c added

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,9 @@
 /**
  * main - Entry point
  * Return: Always 0 (success)
- * */
+ */
 int main(void)
-{       
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	Return(0);
+	return (0);
 }
