853235f99faf68e760d77ffc34a49d602cf85ea6
hadjirozay <ojewandejamiu2016@gmail.com>
2023-10-05 21:11:13 -0700

    hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 1 (success)
+ */
+int main(0)
+{
+        write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\", 59);
+	Return(1):
+}
