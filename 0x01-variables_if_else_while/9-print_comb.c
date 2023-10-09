01da6316833476ee7dec9401139abec4eeb28fb7
hadjirozay <ojewandejamiu2016@gmail.com>
2023-10-09 13:45:52 -0700

    print combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - single numbers
+ * Return: 0 always if successful
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+if (i == 9)
+putchar(i + '0');
+else
+putchar(i);
+putchar(',');
+putchar(' ');
+}
+return (0);
+}
